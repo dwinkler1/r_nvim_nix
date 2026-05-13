422b088da7b6ee1c4628832d70c5ce14d3946318
Daniel <dw@dwinkler.org>
2026-05-13 12:54:10 +1000

    Update R.nvim version to 0.99.4 in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,75 +1,73 @@
+let
+  rnvimVersion = "v0.99.4";
+in
 {
   description = "R.nvim packaging";
 
-  let
-    rnvimVersion = "0.99.4";
-  in
-  {
-    inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-      rnvimsrc = {
-        url = "github:R-nvim/R.nvim/${rnvimVersion}";
-        flake = false;
+    rnvimsrc = {
+      url = "github:R-nvim/R.nvim?ref=${rnvimVersion}";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, rnvimsrc }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        rec {
+          nvimcom = pkgs.rPackages.buildRPackage {
+            pname = "nvimcom";
+            version = "0.99.4";
+            src = "${rnvimsrc}/nvimcom";
+          };
+
+          rnvimserver = pkgs.stdenv.mkDerivation {
+            pname = "rnvimserver";
+            version = "0.99.4";
+            src = rnvimsrc;
+
+            nativeBuildInputs = [ pkgs.gnumake pkgs.gcc ];
+
+            buildPhase = ''
+              runHook preBuild
+              make -C rnvimserver
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              install -Dm755 rnvimserver/rnvimserver $out/bin/rnvimserver
+              runHook postInstall
+            '';
+          };
+
+          r-nvim = pkgs.vimUtils.buildVimPlugin {
+            pname = "R.nvim";
+            version = "0.99.4";
+            src = rnvimsrc;
+          };
+
+          default = r-nvim;
+        });
+
+      overlays.default = final: prev: {
+        nvimcom = self.packages.${final.system}.nvimcom;
+        rnvimserver = self.packages.${final.system}.rnvimserver;
+        r-nvim = self.packages.${final.system}.r-nvim;
       };
     };
-
-    outputs = { self, nixpkgs, rnvimsrc }:
-      let
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ];
-        forAllSystems = nixpkgs.lib.genAttrs systems;
-      in
-      {
-        packages = forAllSystems (system:
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          rec {
-            nvimcom = pkgs.rPackages.buildRPackage {
-              pname = "nvimcom";
-              version = rnvimVersion;
-              src = "${rnvimsrc}/nvimcom";
-            };
-
-            rnvimserver = pkgs.stdenv.mkDerivation {
-              pname = "rnvimserver";
-              version = rnvimVersion;
-              src = rnvimsrc;
-
-              nativeBuildInputs = [ pkgs.gnumake pkgs.gcc ];
-
-              buildPhase = ''
-                runHook preBuild
-                make -C rnvimserver
-                runHook postBuild
-              '';
-
-              installPhase = ''
-                runHook preInstall
-                install -Dm755 rnvimserver/rnvimserver $out/bin/rnvimserver
-                runHook postInstall
-              '';
-            };
-
-            r-nvim = pkgs.vimUtils.buildVimPlugin {
-              pname = "R.nvim";
-              version = rnvimVersion;
-              src = rnvimsrc;
-            };
-
-            default = r-nvim;
-          });
-
-        overlays.default = final: prev: {
-          nvimcom = self.packages.${final.system}.nvimcom;
-          rnvimserver = self.packages.${final.system}.rnvimserver;
-          r-nvim = self.packages.${final.system}.r-nvim;
-        };
-      };
-  };
 }
